e0dada1d24e8f20e65c0c0e8073c450e40043f63
Bruno Vidal <brunovidal27.19@gmail.com>
2023-09-08 16:48:58 -0300

    formatacao do codigo ate o momento

--- FreteApp/Apis/consulta_correio.py
+++ FreteApp/Apis/consulta_correio.py
@@ -1,16 +1,21 @@
 import requests
 
+
 def validar_cep(cep):
     try:
+        # Transforma o cep em string
         cep = str(cep)
+        # Remove os espaços, . e -
         cep = cep.replace(".", "").replace("-", "").replace(" ", "")
-        
+        # Verifica se o cep digitado tem 8 digitos
         if len(cep) != 8 or not cep.isdigit():
-            raise Exception(f'Cep digitado é inválido: {cep}')
-        
+            # Retorna o cep como invalido caso não atendas aos requesitos
+            raise Exception(f"Cep digitado é inválido: {cep}")
+
         return cep
     except Exception as e:
         return str(e)
+
 
 def consultar_endereco(cep):
     status = [200, 404, 505]
@@ -21,15 +26,15 @@ def consultar_endereco(cep):
 
         if response.status_code == 200:
             data = response.json()
-            return data['result']
-        
+            return data["result"]
+
         if response.status_code == 404:
-            raise Exception(f'Erro:{response.status_code}: {cep}')
-        
+            raise Exception(f"Erro:{response.status_code}: {cep}")
+
         if response.status_code == 500:
-            raise Exception(f'Erro:{response.status_code}: Sistema Indisponivel no momento')
+            raise Exception(
+                f"Erro:{response.status_code}: Sistema Indisponivel no momento"
+            )
 
     except Exception as e:
         return str(e)
-
-print(consultar_endereco(5555555))
--- FreteApp/urls.py
+++ FreteApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.base, name='base')
+    path("", views.base, name="base"),
 ]
