37fef45e1931f267e3cba0b904d1c7f8abd5f46b
Bruno Vidal <brunovidal27.19@gmail.com>
2023-10-15 01:54:35 -0300

    teste de redirecionamento de link

--- static/assets/js/teste.js
+++ static/assets/js/teste.js
@@ -18,19 +18,30 @@ itensMenu.forEach(item => {
 
 
 // Selecione e ative os itens do submenu
-var subMenuItens = document.querySelectorAll('.submenu-toggle');
+var subMenuItens = document.querySelectorAll('.nav-link.submenu-toggle');
 
-// Percorra cada item do submenu
 subMenuItens.forEach(subMenuItem => {
-    // Obtenha o link dentro do item do submenu
-    var subMenuLink = subMenuItem.querySelector('.nav-link');
-
-    // Verifique se o href do link corresponde à URL atual
-    if (subMenuLink.getAttribute('href') === window.location.pathname) {
-        // Se corresponder, adicione a classe 'active' ao item do submenu pai
+    // Verifique se o link atual possui a classe 'active'
+    if (subMenuItem.getAttribute('href') === window.location.pathname) {
+        // Adicione a classe 'active' ao link atual
         subMenuItem.classList.add('active');
-    } else {
-        // Se não corresponder, remova a classe 'active' (se existir)
-        subMenuItem.classList.remove('active');
+
+        // Encontre o elemento .collapse dentro deste item do submenu
+        var subMenuCollapse = subMenuItem.nextElementSibling;
+        if (subMenuCollapse.classList.contains('collapse')) {
+            subMenuCollapse.classList.add('show');
+        }
     }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var currentURL = window.location.pathname;
+
+    var subMenuLinks = document.querySelectorAll('.submenu-link');
+
+    subMenuLinks.forEach(function (link) {
+        if (link.getAttribute('href') === currentURL) {
+            link.classList.add('active');
+        }
+    });
 });
