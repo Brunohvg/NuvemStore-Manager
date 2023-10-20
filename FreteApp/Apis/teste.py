25e5a53980808d653e504e3693b70fc6a655b3a3
Bruno Vidal <brunovidal27.19@gmail.com>
2023-10-20 17:56:16 -0300

    zipcode

--- FreteApp/Apis/teste.py
+++ FreteApp/Apis/teste.py
@@ -1,4 +1,4 @@
-"""import requests
+import requests
 from decouple import config  # Certifique-se de que você importou a biblioteca decouple
 
 
@@ -9,35 +9,14 @@ def consultar_endereco_google(end):
     CHAVE = config("TOKEN_GOOGLE")
     CEP_ORIGEM = "30170-130"
     URL_GOOGLE = f"https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins={CEP_ORIGEM}&destinations={end}&key={CHAVE}"
+    base_url = (
+        f"https://maps.googleapis.com/maps/api/geocode/json?address={end}&key={CHAVE}"
+    )
+    response = requests.get(base_url)
 
-    response = requests.get(URL_GOOGLE)
-
-    response_dict = response.json()
-
-    return response_dict
-
-
-print(consultar_endereco_google("78525-000"))
-"""
-import requests
+    if response.status_code == 200:
+        response_dic = response.json()
+        return response_dic["results"][0]["address_components"][0]["long_name"]
 
 
-def consultar_endereco(self, cep_destino):
-    cep_destino_valido = self.validar_cep(cep_destino)
-    if not cep_destino_valido:
-        return {"erro": "CEP de destino inválido."}
-
-    try:
-        url = f"https://viacep.com.br/ws/{cep_destino_valido}/json/"
-        response = requests.get(url=url)
-        response.raise_for_status()
-
-        return response.json()
-
-    except Exception as e:
-        return None
-
-
-re = requests.get("https://viacep.com.br/ws/01001000/json/")
-ree = re.json()
-print(ree)
+print(consultar_endereco_google("rua aimores nossa senhora de fatima sabara"))
